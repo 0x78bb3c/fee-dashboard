c6216ac68150d1fce407ec3ae36797c888a6dd94
alok8bb <alok8bb@gmail.com>
2022-10-19 17:44:19 +0530

    reformat all files

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,51 +2,51 @@ import ReactDOM from "react-dom";
 import { MdClose } from "react-icons/md";
 
 const MODAL_STYLES = {
-  position: "fixed",
-  top: "35%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  backgroundColor: "#FFF",
-  padding: "20px",
-  zIndex: 1000,
-  borderRadius: "10px",
-  display: "flex",
-  flexDirection: "column",
+    position: "fixed",
+    top: "35%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "#FFF",
+    padding: "20px",
+    zIndex: 1000,
+    borderRadius: "10px",
+    display: "flex",
+    flexDirection: "column",
 };
 
 const OVERLAY_STYLES = {
-  position: "fixed",
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  backgroundColor: "rgba(0,0,0,0.7)",
-  zIndex: 1000,
+    position: "fixed",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: "rgba(0,0,0,0.7)",
+    zIndex: 1000,
 };
 
 const Modal = ({ open, children, onClose }) => {
-  console.log(open);
-  if (!open) return null;
+    console.log(open);
+    if (!open) return null;
 
-  return ReactDOM.createPortal(
-    <>
-      <div style={OVERLAY_STYLES}></div>
-      <div style={MODAL_STYLES}>
-        <MdClose
-          size={30}
-          onClick={onClose}
-          style={{
-            color: "black",
-            float: "right",
-            cursor: "pointer",
-            alignSelf: "flex-end",
-          }}
-        />
-        {children}
-      </div>
-    </>,
-    document.getElementById("portal")
-  );
+    return ReactDOM.createPortal(
+        <>
+            <div style={OVERLAY_STYLES}></div>
+            <div style={MODAL_STYLES}>
+                <MdClose
+                    size={30}
+                    onClick={onClose}
+                    style={{
+                        color: "black",
+                        float: "right",
+                        cursor: "pointer",
+                        alignSelf: "flex-end",
+                    }}
+                />
+                {children}
+            </div>
+        </>,
+        document.getElementById("portal")
+    );
 };
 
 export default Modal;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,69 +6,72 @@ import AddCourseModal from "../components/AddCourse";
 import AddFeesModal from "../components/AddFees";
 
 const data = [
-  { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
-  { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
-  { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
-  { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
+    { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
+    { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
+    { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
+    { course_name: "Course 1", pending_fees: 1000, total_fees: 1000 },
 ];
 
 const HomePage = () => {
-  const [feeModalIsOpen, setFeeModalOpen] = useState(false);
-  const [courseModalIsOpen, setCourseModalOpen] = useState(false);
+    const [feeModalIsOpen, setFeeModalOpen] = useState(false);
+    const [courseModalIsOpen, setCourseModalOpen] = useState(false);
 
-  return (
-    <div className="main">
-      <div className="u">
-        <h1>Dashboard</h1>
-        <div className="buttons">
-          <Button
-            bg={"#181818"}
-            onClick={() => {
-              setCourseModalOpen(!courseModalIsOpen);
-            }}
-          >
-            Add Course
-          </Button>
-          <Button
-            bg={"#181818"}
-            onClick={() => {
-              setFeeModalOpen(!feeModalIsOpen);
-            }}
-          >
-            Add Fee
-          </Button>
+    return (
+        <div className="main">
+            <div className="u">
+                <h1>Dashboard</h1>
+                <div className="buttons">
+                    <Button
+                        bg={"#181818"}
+                        onClick={() => {
+                            setCourseModalOpen(!courseModalIsOpen);
+                        }}
+                    >
+                        Add Course
+                    </Button>
+                    <Button
+                        bg={"#181818"}
+                        onClick={() => {
+                            setFeeModalOpen(!feeModalIsOpen);
+                        }}
+                    >
+                        Add Fee
+                    </Button>
+                </div>
+            </div>
+            <Modal open={feeModalIsOpen} onClose={() => setFeeModalOpen(false)}>
+                <AddFeesModal courses={data} />
+            </Modal>
+            <Modal
+                open={courseModalIsOpen}
+                onClose={() => setCourseModalOpen(false)}
+            >
+                <AddCourseModal />
+            </Modal>
+            <div className="b">
+                <table className="styled-table">
+                    <thead>
+                        <tr>
+                            <th>Course</th>
+                            <th>Pending Fee</th>
+                            <th>Total Fee</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data.map((val, key) => {
+                            return (
+                                <tr key={key}>
+                                    <td>{val.course_name}</td>
+                                    <td>{val.pending_fees}</td>
+                                    <td>{val.total_fees}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            </div>
         </div>
-      </div>
-      <Modal open={feeModalIsOpen} onClose={() => setFeeModalOpen(false)}>
-        <AddFeesModal courses={data} />
-      </Modal>
-      <Modal open={courseModalIsOpen} onClose={() => setCourseModalOpen(false)}>
-        <AddCourseModal />
-      </Modal>
-      <div className="b">
-        <table className="styled-table">
-          <thead>
-            <tr>
-              <th>Course</th>
-              <th>Pending Fee</th>
-              <th>Total Fee</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data.map((val, key) => {
-              return (
-                <tr key={key}>
-                  <td>{val.course_name}</td>
-                  <td>{val.pending_fees}</td>
-                  <td>{val.total_fees}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default HomePage;
